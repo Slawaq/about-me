d50f54731bad3f0f9bea771d271317cf70f973d5
Vyacheslav Pytel <dereckvk@gmail.com>
2016-12-20 12:12:06 +0200

    Fix browserSync for prod

--- src/routing.js
+++ src/routing.js
@@ -14,19 +14,21 @@ var pages = [{
   name: 'contacts'
 }]
 
+let isDev = req => ['127.0.0.1','localhost'].indexOf(req.hostname) !== -1
+
 module.exports = app => {
   app.get(pages.map(x => x.path), (req, res) => {
-    res.render('index', { page: req.path, pages })
+    res.render('index', { page: req.path, pages, isDev: isDev(req) })
   })
 
   app.use((req, res, next) => {
-    res.render('404', { status: 404, url: req.url });
-  });
+    res.render('404', { status: 404, url: req.url })
+  })
 
   app.use((req, res, next) => {
     res.render('500', {
       code: err.status || 500, 
       error: err
-    });
-  });
+    })
+  })
 }
