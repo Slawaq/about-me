f611193bf827a7ecc9f93a07994aecf93c3ca992
Vyacheslav Pytel <dereckvk@gmail.com>
2016-12-20 02:53:31 +0200

    Add basic routing

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 'use strict'
 
 const express = require('express')
+const compression = require('compression')
 const config = require('./config')
 const setupRouting = require('./src/routing')
+
 const app = express()
+app.use(express.static('views'))
 
 app.set('view engine', 'pug')
 setupRouting(app)
 
-app.listen(config.port, () => console.log('SERVER STARTED!'))
+app.listen(config.port)
--- src/routing.js
+++ src/routing.js
@@ -1,15 +1,32 @@
 'use strict'
 
+var pages = [{
+  path: '/', 
+  name: 'home'
+},{
+  path: '/biography', 
+  name: 'biography'
+},{
+  path: '/products', 
+  name: 'products'
+},{
+  path: '/contacts', 
+  name: 'contacts'
+},{
+  path: '/magic', 
+  name: 'magic'
+}]
+
 module.exports = app => {
-  app.get('/', function (req, res) {
-    res.render('index', { title: 'Hey', message: 'Hello there!' })
+  app.get(pages.map(x => x.path), (req, res) => {
+    res.render('index', { page: req.path, pages })
   })
 
-  app.use(function(req, res, next){
+  app.use((req, res, next) => {
     res.render('404', { status: 404, url: req.url });
   });
 
-  app.use(function(req, res, next){
+  app.use((req, res, next) => {
     res.render('500', {
       code: err.status || 500, 
       error: err
